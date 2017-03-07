1f648fe5c7d8101dda46b84acbd6fbd335c48d7b
msachi <marchee88@gmail.com>
2017-03-07 15:04:10 +0000

    Modify getUser helper test to check for the output

---
+++ test/sampleActivityData.js
@@ -0,0 +1,11 @@
+module.exports = {
+  profile: {
+    someValue: true,
+    someothervalue: false
+  },
+  receipt: {
+    remember_me_id: 'bananasorangesapplesstrawberriespineapplesblueberriesraspberries',
+    outcome: 'SUCCESS',
+    timestamp: '2017-03-03T14:10:17Z'
+  }
+};
