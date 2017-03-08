d6a9c16e3543bb0e8e3c69f5822b9e35b46e5bd4
msachi <marchee88@gmail.com>
2017-03-08 12:11:22 +0000

    Add admin authentication in dashboard.js

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,24 +1,67 @@
+const fs = require('fs');
+const path = require('path');
+const YotiClient = require('yoti-node-sdk');
+const userTable = require('../database/tables/users');
+const userHelper = require('../helpers/userHelper');
 const retrieveData = require('./../database/tables/retrieveRequests.js')
-const databaseError = "There was a problem processing your data, please try again. If this problem persists, contact our technical team.";
+
+const CLIENT_SDK_ID = "8a4dcb2a-9ed6-4d44-9a55-12b581bb5e64";
+const PEM = fs.readFileSync(path.join(__dirname, '../../keys/help-access-security.pem'));
+const yotiClient = new YotiClient(CLIENT_SDK_ID, PEM);
+const loginError = "There was a problem accessing your Yoti, please log in again. If this problem persists, contact our technical team.";
+const databaseError = "There was a problem retrieving data, please try again. If this problem persists, contact our technical team.";
+const authenticationError = "You are not authenticated to access the data. If you are a Centrepoint administrator, contact our technical team."
 
 module.exports = {
   method: 'GET',
   path:'/dashboard',
-  handler: (req, reply) => {
-
-    retrieveData.retrieve(function (err, data) {
-      if (err) {
-        console.log("Error retrieving requests: ", err);
+  config: {
+    auth: {
+      strategy: 'base'
+    },
+    handler: (req, reply) => {
+      let token = req.auth.credentials.auth;
+      if(!token) {
+        console.log("No token provided");
         return reply.view('error', {
-          error: databaseError
+          error : loginError
         });
       }
-      console.log(data.rows);
-
-      reply.view('dashboard', { requests: data.rows });
-    });
+      yotiClient
+      .getActivityDetails(token)
+      .then((activityDetails) => {
+        let user = userHelper.getUser(activityDetails);
+        userTable.queryUserId(user, function (err, data) {
+          if (err) {
+            console.log("Error adding user: ", err);
+            return reply.view('error', {
+              error : databaseError
+            });
+          }
+          if (! data[0].admin) {
+            return reply.view('error', {
+              error : authenticationError
+            });
+          }
+          retrieveData.retrieve(function (err, data) {
+            if (err) {
+              console.log("Error retrieving requests: ", err);
+              return reply.view('error', {
+                error: databaseError
+              });
+            }
+            reply.view('dashboard', { requests: data.rows });
+          });
+        });
+      }).catch((err) => {
+        console.error(err);
+        reply.view('error', {
+          error : yotiError
+        });
+      })
+    }
   }
-}
+};
 //
 //     reply.view('dashboard',
 //     { requests: [
