5d7b00a3b303abde62c6b0e7d52e333241cc8bab
lucymonie <lucy.monie.hall@gmail.com>
2017-03-11 18:28:04 +0000

    Removes long chains of .firstElementChild from clientside js

--- public/requestSwitch.js
+++ public/requestSwitch.js
@@ -1,9 +1,9 @@
 let requests = document.getElementsByClassName("request");
 
 for(i = 0; i < requests.length; i++){
-  let requestSwitch = requests[i].firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild;
-  let active = requests[i].firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.checked;
-  let id = requests[i].lastElementChild.lastElementChild.firstElementChild.getAttribute('data-value');
+  let requestSwitch = requests[i].querySelector('.label');
+  let active = requests[i].querySelector('input').checked;
+  let id = requests[i].querySelector('.requestId').getAttribute('data-value');
   requestSwitch.addEventListener('click', function(e) {
     window.location.href = `/status?id=${id}&active=${active?false:true}`;
   });
