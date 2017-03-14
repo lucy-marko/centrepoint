f1a84f3bc93a590910bce95be90cc974b1a32daf
lucymonie <lucy.monie.hall@gmail.com>
2017-03-14 16:32:44 +0000

    Updates phone helper so it deals with numbers in separate cases

--- src/helpers/phoneHelper.js
+++ src/helpers/phoneHelper.js
@@ -1,6 +1,9 @@
 module.exports.formatMobile = function (user) {
   let newUser = Object.assign({}, user);
-  let numStr = `0${user.phoneNumber.slice(2)}`;
+  let numStr = newUser.phoneNumber;
+  if (numStr.slice(0,2) === '44') {
+    numStr = `0${user.phoneNumber.slice(2)}`;
+  }
   number = numStr.replace(/\s+/g, '');
   newUser.localNumber = number;
   return newUser;
--- test/helpers.test.js
+++ test/helpers.test.js
@@ -16,7 +16,25 @@ test('check that getFirstName function returns a correctly formatted first name'
   t.end();
 });
 
-test('check that formatMobile function returns a correct mobile number', function (t) {
+test('check that formatMobile function returns a correct mobile number if it starts with 0', function (t) {
+  let newUser = phoneHelper.formatMobile({ phoneNumber: '07890444333'});
+  t.equal(newUser.localNumber, '07890444333');
+  t.end();
+});
+
+test('check that formatMobile function returns a correct mobile number if it starts with 44', function (t) {
+  let newUser = phoneHelper.formatMobile({ phoneNumber: '447890444333'});
+  t.equal(newUser.localNumber, '07890444333');
+  t.end();
+});
+
+test('check that formatMobile function returns a correct mobile number if it starts with 0 and contains spaces', function (t) {
+  let newUser = phoneHelper.formatMobile({ phoneNumber: '07890 444 333'});
+  t.equal(newUser.localNumber, '07890444333');
+  t.end();
+});
+
+test('check that formatMobile function returns a correct mobile number if it starts with 44 and contains spaces', function (t) {
   let newUser = phoneHelper.formatMobile({ phoneNumber: '447890 444 333'});
   t.equal(newUser.localNumber, '07890444333');
   t.end();
