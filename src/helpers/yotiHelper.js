c383c43089312a0aaaf8cb9f7980705ec35a51e6
msachi <marchee88@gmail.com>
2017-03-15 16:27:19 +0000

    Deploy

--- src/helpers/yotiHelper.js
+++ src/helpers/yotiHelper.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const env = require('env2');
-env('./config.env');
-
 const YotiClient = require('yoti-node-sdk');
 const CLIENT_SDK_ID = process.env.SDK;
 const PEM = fs.readFileSync(path.join(__dirname, '../../keys_pem/help-access-security.pem'));
--- src/server.js
+++ src/server.js
@@ -14,11 +14,7 @@ const deployStatus = process.env.NODE_ENV === 'PRODUCTION';
 const server = new hapi.Server();
 
   server.connection ({
-    port: process.env.PORT || 9443,
-    tls: {
-      key: fs.readFileSync(path.join(__dirname, '../keys_tls/key.pem')),
-      cert: fs.readFileSync(path.join(__dirname, '../keys_tls/cert.pem'))
-    }
+    port: process.env.PORT || 9443
   });
 
 server.register([inert, vision, CookieAuth], (err) => {
