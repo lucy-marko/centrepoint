86ce5fcd0509c07422539dfef809f06bf979906c
Joaquim d'Souza <joaquimds@gmail.com>
2017-03-27 16:58:39 +0100

    remove creds from sql file

--- src/database/build_database.sql
+++ src/database/build_database.sql
@@ -10,9 +10,6 @@ CREATE TABLE users (
   admin BOOLEAN
 );
 
-INSERT INTO users (user_id, given_names, family_name, birth_date, phone_number, admin) VALUES ('xFv5nFhg74HjsolcjDeDDhhbvgcf08G9f4Xf1', 'Nori', 'Denes', '1/1/2000', 07123456789, false);
-INSERT INTO users (user_id, given_names, family_name, birth_date, phone_number, admin) VALUES ('Re5xYlrYsu8ctaxPN2g7zZYGHSKOHotA6UYABPmadPvMaPs3ssziv0iEBd75zfi2', 'MARKO', 'SUSTARSIC', '1988-02-21', 447811110722, false);
-
 DROP TABLE IF EXISTS requests cascade;
 
 CREATE TABLE requests (
@@ -28,6 +25,3 @@ CREATE TABLE requests (
   time_stamp TIMESTAMP WITH TIME ZONE,
   user_id TEXT REFERENCES users (user_id)
 );
-
-INSERT INTO requests (rental_reference, rental_arrears, rental_history, other_requests, email, street, town, postcode, time_stamp, user_id) VALUES (TRUE, FALSE, FALSE, 'also some other report', 'hello@gmail.com', NULL, NULL, NULL, current_timestamp, 'xFv5nFhg74HjsolcjDeDDhhbvgcf08G9f4Xf1');
-
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('reading test.js')
+
+module.exports = {
+	prop: 'Test property'
+}
