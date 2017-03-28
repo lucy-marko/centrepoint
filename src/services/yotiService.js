a98d8829f53d993a10a4a9d8766ff23995661910
msachi <marchee88@gmail.com>
2017-03-28 13:18:14 +0100

    Refactor PEM file dependency

--- src/services/yotiService.js
+++ src/services/yotiService.js
@@ -3,13 +3,19 @@ const path = require('path');
 const env = require('env2');
 env('./config.env');
 
+const isProduction = process.env.NODE_ENV === 'PRODUCTION';
+
 const YotiSDK = require('yoti-node-sdk');
 let yotiClient;
 
 module.exports.getClient = function() {
   if (! yotiClient) {
     const CLIENT_SDK_ID = process.env.SDK;
-    const PEM = process.env.PEM || fs.readFileSync(path.join(__dirname, '../../key_pem/help-access-security.pem'));
+    if (isProduction) {
+      const PEM = process.env.PEM;
+    } else {
+      const PEM = fs.readFileSync(path.join(__dirname, '../../key_pem/help-access-security.pem'));
+    }
     yotiClient = new YotiSDK(CLIENT_SDK_ID, PEM);
   };
   return yotiClient;
