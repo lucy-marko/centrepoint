cac97483229f7ffefad77739740eb23e39c4a0c3
msachi <marchee88@gmail.com>
2017-03-30 14:24:14 +0100

    Solve merge error

--- test/suite/_setup.test.js
+++
@@ -1,6 +0,0 @@
-const yotiService = require('../../services/yotiService.js');
-const mockYotiClient = require('../helpers/mockYotiClient.js');
-
-yotiService.getClient = function() {
-  return mockYotiClient;
-};
